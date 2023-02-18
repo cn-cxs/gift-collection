d47a71d16e76c39b8694b8f4d2aa243218cabb5f
C1 <cxs520163@163.com>
2023-02-18 17:35:55 +0800

    change code

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -1,23 +1,49 @@
 // 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+
+const birthday = new Date('1996/2/18 0:0:0');
+const startday = new Date('2022/11/30 0:0:0');
+const now = new Date();
+const days1 = getDiffDays(startday, now);
+const days2 = getDiffDays(birthday, now);
+const ages = getDiffYears(birthday,now);
+
+function getDiffYears(date1,date2) {
+    return date2.getFullYear()-date1.getFullYear();
+}
+
+function getDiffDays(date1,date2) {
+    const date3=date2.getTime()-date1.getTime(); //时间差秒
+    //计算出相差天数
+    return Math.floor(date3/(24*3600*1000));
+}
+function isBirthDay(){
+    if(birthday.getMonth()==now.getMonth()&&birthday.getDate()==now.getDate()){
+        return true;
+    }
+    return false;
+}
+// console.log(days1,days2,ages)
+const text0 = isBirthDay()?"今天是你"+ages+"岁的生日":"今天是你出生的第"+days2+"天";
+const text1 = "第"+days1+"天！";
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的姑娘",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日！",
-        "这两年来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
+        "送给我们",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "聪明的萌萌同学",  // 同上...
+        text0,
+        "这是我们认识的",
+        text1,
+        // "这两年来，我见过你",
+        // "奇怪的样子",
+        // "可爱的样子",
+        // "生气的样子",
+        // "都是我喜欢的样子",
+        // "对了，礼物收全了吗？",
         "今年要吃好好的",
         "喂饱饱的",
-        "然后才有力气干活",
-        "和想我！",
+        "然后才有力气学习哦",
+        // "和想我！",
         "生日快乐~~",
     ],
     /**
@@ -30,21 +56,21 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
-        "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        "送给我们": "./imgs/songxinxin.jpeg",
+        "聪明的萌萌同学": "./imgs/nothing.jpg",
+        text0: "./imgs/shengri.jpeg",
+        "这是我们认识的": "./imgs/yangzi.jpeg",
+        text1: "./imgs/yangzi.jpeg",
+        // "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        // "奇怪的样子": "./imgs/nothing.jpg",
+        // "可爱的样子": "./imgs/nothing.jpg",
+        // "生气的样子": "./imgs/nothing.jpg",
+        // "都是我喜欢的样子": "./imgs/nothing.jpg",
+        // "对了，礼物收全了吗？": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/chihaode.png",
         "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
+        "然后才有力气学习哦": "./imgs/chuo.gif",
+        // "和想我！": "./imgs/nothing.jpg",
         "生日快乐~~": "./imgs/nothing.jpg"
     },
     
@@ -61,5 +87,5 @@ var config = {
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '曹童鞋~'
 };
